7d93aff5c340a9184236f22ac41437c2f71aaa02
K S Sarika <sarika.ks.official@gmail.com>
2024-02-29 14:46:56 +0530

    Changed to Driving

--- client/src/pages/SearchDocs/SearchDocs.js
+++ client/src/pages/SearchDocs/SearchDocs.js
@@ -114,7 +114,7 @@ const SearchDocs = () => {
       // an arbitrary start will always be the same
       // only the end or destination will change
       const query = await fetch(
-        `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${API_TOKEN}`,
+        `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${API_TOKEN}`,
         { method: 'GET' }
       );
       const json = await query.json();
