f5a313d6f4d71afa8900c68ccb9e05a91c4db1ef
K S Sarika <sarika.ks.official@gmail.com>
2024-02-29 14:45:40 +0530

    Error Handling Done

--- client/src/pages/SearchDocs/SearchDocs.js
+++ client/src/pages/SearchDocs/SearchDocs.js
@@ -108,52 +108,58 @@ const SearchDocs = () => {
   };
 
   async function getRoute(start, end) {
-    console.log('Getting route');
-    // make a directions request using cycling profile
-    // an arbitrary start will always be the same
-    // only the end or destination will change
-    const query = await fetch(
-      `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${API_TOKEN}`,
-      { method: 'GET' }
-    );
-    const json = await query.json();
-    const data = json.routes[0];
-    console.log(data);
-    const route = data.geometry.coordinates;
-    const geojson = {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        coordinates: route
-      }
-    };
-    // if the route already exists on the map, we'll reset it using setData
-    if (map.current.getSource('route')) {
-      map.current.getSource('route').setData(geojson);
-    }
-    // otherwise, we'll make a new request
-    else {
-      map.current.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-          type: 'geojson',
-          data: geojson
-        },
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#3887be',
-          'line-width': 5,
-          'line-opacity': 0.75
+    try {
+      console.log('Getting route');
+      // make a directions request using cycling profile
+      // an arbitrary start will always be the same
+      // only the end or destination will change
+      const query = await fetch(
+        `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${API_TOKEN}`,
+        { method: 'GET' }
+      );
+      const json = await query.json();
+      const data = json.routes[0];
+      console.log(data);
+      const route = data.geometry.coordinates;
+      const geojson = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: route
         }
-      });
+      };
+      // if the route already exists on the map, we'll reset it using setData
+      if (map.current.getSource('route')) {
+        map.current.getSource('route').setData(geojson);
+      }
+      // otherwise, we'll make a new request
+      else {
+        map.current.addLayer({
+          id: 'route',
+          type: 'line',
+          source: {
+            type: 'geojson',
+            data: geojson
+          },
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          paint: {
+            'line-color': '#3887be',
+            'line-width': 5,
+            'line-opacity': 0.75
+          }
+        });
+      }
+      // add turn instructions here at the end
+    } catch (error) {
+      console.error('Error fetching route:', error);
+      // Handle the error here, such as displaying an error message to the user
     }
-    // add turn instructions here at the end
   }
+  
 
   
   const fetchDirection = async (latitude, longitude) => {
