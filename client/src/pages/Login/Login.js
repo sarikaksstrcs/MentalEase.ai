70731afff92f9c948a98f88bfb1d56d291f94524
K S Sarika <sarika.ks.official@gmail.com>
2024-05-01 22:34:37 +0530

    Login UI change

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -36,20 +36,8 @@ const Login = () => {
   };
   return (
      <div className="w-full h-screen flex overflow-hidden">
-      <div className="mt-32 pl-5 w-3/5 ">
-      <h1 class="text-5xl leading-tight font-semibold pt-2 mt-4 text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-sky-400 
-                  sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl    ">
-      MentalHealth.ai
-        </h1>
-        <h1 className="mt-4 text-2xl font-normal ">
-             Where AI Meets Empathy: Your Mental Health Chat Companion In Malayalam
-        </h1>
-        <div className=" pt-4 justify-center flex">
-         <img src={wallpaper} className="w-[300px] animate-pulse  " />
-        </div>
-      </div>
-      <div className="w-2/5 bg-[#02203c] flex justify-between text-sky-300">
-      <div className="p-8 w-full min-h-[300px] px-24 self-center">
+      <div className="w-2/5 bg-black flex justify-between text-white">
+        <div className="p-8 w-full min-h-[300px] px-24 self-center">
           <div
             className="flex flex-col justify-center gap-6"
             // onSubmit={(e) => login(e)}
@@ -58,19 +46,19 @@ const Login = () => {
             <p className="text-md font-medium">
               Open the Door to Mental Wellness.
             </p>
-            <div className="bg-sky-900 shadow p-2 flex items-center rounded-md ">
-              <MdEmail className="text-sky-500 mr-2" size={18} />
+            <div className="border border-opacity-30 shadow p-2 flex items-center rounded-full ">
+              <MdEmail className="text-white mr-2" size={18} />
               <input
                 type="text"
-                placeholder="Enter email"
+                placeholder="Enter Email"
                 className="p-1 w-full text-sm outline-none bg-inherit"
                 value={data.email}
                 onChange={(e) => setData({ ...data, email: e.target.value })}
               />
             </div>
 
-            <div className="bg-sky-900 shadow p-2 flex items-center rounded-md">
-              <MdPassword className="text-sky-500 mr-2" size={18} />
+            <div className="border border-opacity-25 shadow p-2 flex items-center rounded-full   ">
+              <MdPassword className="text-white mr-2" size={18} />
               <input
                 type="password"
                 placeholder="Enter password"
@@ -82,14 +70,26 @@ const Login = () => {
 
             <button
               onClick={() => login()}
-              className="bg-gradient-to-bl from-sky-600 to-sky-300 bg-[position:_0%_0%] hover:bg-[position:_100%_100%] bg-[size:_200%] transition-all duration-500 text-[#02203c] p-3 rounded-md"
+              className="bg-white hover:bg-gray-400 hover:text-white bg-[position:_0%_0%] hover:bg-[position:_100%_100%] bg-[size:_200%] transition-all duration-500 text-black p-3 rounded-md"
             >
               {logging ? "Logging in..." : "Login"}
             </button>
-            <Link to="/signup" className="text-sky-600 mt-4 font-medium">
+            <Link to="/signup" className="text-white mt-4 font-medium">
               Do not have an account? Sign Up
             </Link>
           </div>
+        </div>
+      </div>
+      <div className="mt-32 pl-5 w-3/5 ">
+        <h1 class="text-5xl leading-tight font-semibold pt-2 mt-4 text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-sky-400 
+                  sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl    ">
+          MentalHealth.ai
+        </h1>
+        <h1 className="mt-4 text-2xl font-normal ">
+             Where AI Meets Empathy: Your Mental Health Chat Companion In Malayalam
+        </h1>
+        <div className=" pt-4 justify-center flex">
+         <img src={wallpaper} className="w-[300px] animate-pulse  " />
         </div>
       </div>
     </div>
