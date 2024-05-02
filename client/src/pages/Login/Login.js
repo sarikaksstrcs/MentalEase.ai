b92b3d205bc7accc2cc06cad2799058cad5e2878
K S Sarika <sarika.ks.official@gmail.com>
2024-05-02 21:25:21 +0530

    UI change

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -85,8 +85,8 @@ const Login = () => {
                   sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl    ">
           MentalHealth.ai
         </h1>
-        <h1 className="mt-4 text-2xl font-normal ">
-             Where AI Meets Empathy: Your Friend During Tough Times
+        <h1 className="mt-4 text-2xl font-bold font-serif ">
+             Your Mental Wellness Ally, Always by Your Side
         </h1>
         <div className=" pt-4 justify-center flex">
          <img src={wallpaper} className="w-[300px] animate-pulse  " />
